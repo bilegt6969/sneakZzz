1561c849caa1a72e922a2a2a99b1dbcadeb55dae
“Bilegt6969” <bilegt6969@gmail.com>
2023-09-08 23:40:22 +0800

    silicon valley

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <Analytics />
+
     </html>
+    
   )
 }
